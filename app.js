b455b7ff739e3d0c2d8d2e02553820754dc34a27
Egiemilhas <egiemilhas@gmail.com>
2023-12-25 21:42:06 +0700

    Terbaru

---
+++ app.js
@@ -0,0 +1,26 @@
+function updateAutomatedValues() {
+  const assetType = document.getElementById('asset-type').value;
+  const assetPrice = parseFloat(document.getElementById('asset-price').value);
+
+  if (isNaN(assetPrice)) {
+    alert('Masukkan angka nya Lurrrr');
+    return;
+  }
+
+  let executedPrice, mt5Price;
+
+  if (assetType === 'btc') {
+    executedPrice = assetPrice - 270;
+    mt5Price = assetPrice - 25;
+  } else if (assetType === 'gold') {
+    executedPrice = assetPrice - 20;
+    mt5Price = assetPrice - 3;
+  }
+
+  let result = '';
+
+  result += `Harga di MT5: ${mt5Price.toFixed(0)}<br>`;
+  result += `Harga Tereksekusi: ${executedPrice.toFixed(0)}<br>`;
+
+  document.getElementById('result').innerHTML = result;
+}
